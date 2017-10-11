594f4e5dcc581bd1c8e06bdfa3e8f977196c109f
Justin <justinruan@gmail.com>
2017-10-11 22:12:46 +0800

    ki 67

--- DetectCancer/detect_ki67.py
+++ DetectCancer/detect_ki67.py
@@ -8,7 +8,7 @@ import utils
 import numpy as np
 from matplotlib import pyplot as plt
 from skimage import io, filters, color, morphology, feature, measure, segmentation, transform
-
+from scipy import signal
 
 def read_ki67(scale, left_x, top_y, width, height):
     slide = DigitalSlide()
@@ -42,37 +42,57 @@ def read_mask(mask, left_x, top_y, width, height):
     slide.release_slide_pointer()
 
     ImageHeight, ImageWidth = mask.shape
-    large_mask = transform.resize(mask, (ImageHeight * 10, ImageWidth * 10))
+    large_mask = transform.resize(mask, (ImageHeight * 8, ImageWidth * 8))
     roi_mask = large_mask[top_y:top_y + height, left_x:left_x + width]
+    roi_mask = roi_mask > 0.175
     return roi_image, roi_mask
 
 
+def positive_hotmap(ki67, notki67, k):
+    m = np.ones((k, k), dtype=np.float) / (k * k)
+
+    mean_ki67 = signal.convolve2d(ki67, m)
+    mean_notki67 = signal.convolve2d(notki67, m)
+
+    # m = signal.gaussian(50, 10.0)
+    result = (mean_ki67) / (mean_notki67 + 1e-3)
+
+    SE = morphology.disk(3)
+    result = morphology.dilation(result, SE)
+    return result
+
+
+
 if __name__ == '__main__':
-    mask = np.fromfile("he_result_img.bin", dtype=np.float)  # (1834, 2145)
+    mask = np.fromfile("he_result_img (2292, 2681).bin", dtype=np.float)  # (1834, 2145) (2292, 2681)
+    #
+    mask = np.reshape(mask, (2292, 2681))
 
-    mask = np.reshape(mask, (1834, 2145))
+    he_img, roi_mask = read_mask(mask, (1772 - 200) * 4, (2125 - 200) * 4, 400 * 4, 300 * 4)
 
-    full_img, roi_mask = read_mask(mask, (1772 - 200) * 4, (2125 - 200) * 4, 400 * 4, 300 * 4)
+    ki67_img, ki67, notki67 = read_ki67(2.26 * 4, (963 - 200) * 4, (1589 - 200) * 4, 400 * 4, 300 * 4)
+    ki67[~roi_mask] = 0
 
-    # full_img, ki67, notki67 = read_ki67(2.26 * 4, (963 - 200)*4, (1589 - 200)*4, 400*4, 300*4)
-    # registration()
+    result = positive_hotmap(ki67, notki67, 9)
 
-
-
-
-    fig, axes = plt.subplots(1, 2, figsize=(4, 3))
+    fig, axes = plt.subplots(2, 3, figsize=(4, 3))
     ax = axes.ravel()
+    ax[0].imshow(ki67_img)
+    ax[0].set_title("ki 67_img")
 
-    ax[0].imshow(full_img)
-    ax[0].set_title("full_img")
+    ax[1].imshow(ki67_img)
+    ax[1].contour(ki67, [0.5], linewidths=0.5, colors='r')
+    ax[1].contour(notki67, [0.5], linewidths=0.5, colors='b')
+    ax[1].set_title("Enhanced ki 67")
 
-    ax[1].imshow(roi_mask)
-    ax[1].set_title("roi_mask")
-    # ax[1].imshow(full_img)
-    # ax[1].contour(ki67, [0.5], linewidths=0.5, colors='r')
-    # ax[1].contour(notki67, [0.5], linewidths=0.5, colors='b')
-    # ax[1].set_title("C")
+    ax[2].imshow(he_img)
+    ax[2].set_title("he_img")
 
+    ax[3].imshow(roi_mask)
+    ax[3].set_title("roi_mask")
+
+    ax[4].imshow(result, cmap=plt.cm.jet)
+    ax[4].set_title("result hotmap of ki 67")
     for a in ax.ravel():
         a.axis('off')
 
