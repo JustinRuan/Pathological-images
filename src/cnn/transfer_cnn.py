75b4c19afeeb1715cf79fa3f069749b2f202a7b8
Justin <justinruan@gmail.com>
2018-06-01 00:54:58 +0800

    完成迁移学习的功能

--- src/cnn/transfer_cnn.py
+++ src/cnn/transfer_cnn.py
@@ -17,6 +17,7 @@ from skimage import io
 from sklearn.svm import NuSVC, SVC
 from sklearn.externals import joblib
 import pandas as pd
+from sklearn.model_selection import GridSearchCV
 
 class transfer_cnn(object):
 
@@ -42,7 +43,7 @@ class transfer_cnn(object):
         # 改变维度的顺序，由原始图片(28,28,3)变为(3,28,28)
         transformer.set_transpose('data', (2, 0, 1))
         # 减去均值，若训练时未用到均值文件，则不需要此步骤
-        transformer.set_mean('data', np.array([103.939, 116.779, 123.68]))
+        # transformer.set_mean('data', np.array([103.939, 116.779, 123.68]))
         # 缩放到【0，255】之间
         transformer.set_raw_scale('data', 255)
         # 交换通道，将图片由RGB变为BGR
@@ -83,13 +84,14 @@ class transfer_cnn(object):
             output = net.forward()
 
             for fvector2 in net.blobs[self.extract_layer_name].data:
-                cnn_features.append(fvector2)
+                cnn_features.append(fvector2.copy()) # 神坑，花费一天时间爬出
 
                 print("{} --> tag:{}, f1:{}, f2:{}".format(n, tags[n], glcm_features[n][0:3],fvector2[0:4]))
                 n += 1
 
         f.close()
-
+        # np.save("features",{"cnn":cnn_features,"tags":tags})
+        # np.savetxt('features.txt', np.column_stack((tags,cnn_features)), delimiter=',',fmt='%.6e')
         return glcm_features, cnn_features, tags
 
     def select_features(self, cnn_features, tags):
@@ -102,7 +104,7 @@ class transfer_cnn(object):
         # print(abs(diff.iloc[0,:] - diff.iloc[1,:]))
         print(absDiff[:100])
 
-        top_index = absDiff[:100].index.values - 1 # 将index 从1开始移到0开始
+        top_index = absDiff[:100].index.values - 1 # 将index 从1开始的，0是tag的位置
         index_filename = "{}_{}".format(self.transfer_model, "top_index")
         np.save(index_filename, top_index)
         return top_index
@@ -112,13 +114,14 @@ class transfer_cnn(object):
         y = tags
 
         new_cnn_features = np.array(cnn_features)[:, top_index]
-        # X = np.column_stack((glcm_features, new_cnn_features))
-        X = new_cnn_features
+        X = np.column_stack((glcm_features, new_cnn_features))
+        # X = new_cnn_features
 
-        # clf = NuSVC(nu=0.05, kernel='rbf', probability=True) # nu=0.5,
-        # clf = SVC(kernel='poly', degree=3, C=1)
-        clf = SVC(kernel='rbf', gamma=0.7, C=1)
+        parameters = {'kernel':('linear', 'rbf'), 'C':[1, 10], 'gamma':[0,10]}
+        clf = GridSearchCV(SVC(), parameters, cv=5)
+
         rf = clf.fit(X ,y)
+        print("Best param: {} \t Beat score: {}".format(clf.best_params_, clf.best_score_))
 
         model_file = "{}/models/svm_{}.model".format(self._params.PROJECT_ROOT, self.model_filename)
         joblib.dump(rf, model_file)
@@ -133,14 +136,14 @@ class transfer_cnn(object):
     def test_svm(self, test_filename):
 
         index_filename = "{}_{}.npy".format(self.transfer_model, "top_index")
-        top100_index = np.load(index_filename)
+        top_index = np.load(index_filename)
         classifier = self.load_svm_model()
 
         glcm_features, cnn_features, expected_tags = self.loading_data(test_filename)
 
-        new_cnn_features = np.array(cnn_features)[:, top100_index]
-        # features = np.column_stack((glcm_features, new_cnn_features))
-        features = new_cnn_features
+        new_cnn_features = np.array(cnn_features)[:, top_index]
+        features = np.column_stack((glcm_features, new_cnn_features))
+        # features = new_cnn_features
         predicted_tags = classifier.predict(features)
 
         # predicted_result = classifier.predict_proba(features)
--- src/test/cnn/test_transfer_cnn.py
+++ src/test/cnn/test_transfer_cnn.py
@@ -18,15 +18,15 @@ class Test_transfer_cnn(unittest.TestCase):
         c = Params.Params()
         c.load_config_file("D:/CloudSpace/DoingNow/WorkSpace/PatholImage/config/justin.json")
         # tc = transfer_cnn.transfer_cnn(c, "my_googlenet.caffemodel",
-        #                                "my_googlenet_deploy.prototxt", 'loss3/classifier', "ZoneA")
+        #                                "my_googlenet_deploy.prototxt", 'loss3/classifier')
         tc = transfer_cnn.transfer_cnn(c, "bvlc_googlenet.caffemodel",
                                        "deploy_GoogLeNet.prototxt", 'loss3/classifier')
 
-        glcm_features, cnn_features, tags = tc.loading_data("Small500_train.txt")
+        glcm_features, cnn_features, tags = tc.loading_data("Small_train.txt")
         top_index = tc.select_features(cnn_features, tags)
 
         tc.train_svm(glcm_features,cnn_features,tags,top_index)
-        tc.test_svm("Small500_train.txt")
+        tc.test_svm("Small_test.txt")
         return
 
     def test_testSVM(self):
@@ -35,8 +35,8 @@ class Test_transfer_cnn(unittest.TestCase):
         tc = transfer_cnn.transfer_cnn(c, "bvlc_googlenet.caffemodel",
                                        "deploy_GoogLeNet.prototxt", 'loss3/classifier')
         # tc = transfer_cnn.transfer_cnn(c, "my_googlenet.caffemodel",
-        #                                "my_googlenet_deploy.prototxt", 'loss3/classifier', "ZoneA")
-        tc.test_svm("Small500_train.txt")
+        #                                "my_googlenet_deploy.prototxt", 'loss3/classifier')
+        tc.test_svm("Small_test.txt")
         return
 
     def test_01(self):
